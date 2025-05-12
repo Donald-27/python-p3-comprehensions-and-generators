a4b16bb97a866793f149993afd472a71544b65b7
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-12 23:01:48 +0300

    modified list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -5,6 +5,8 @@
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
+
 # Function that appends an exclamation mark to each sentence in the list using list comprehension
+
 def make_exclamation(sentence_list):
     return [sentence + '!' for sentence in sentence_list]
